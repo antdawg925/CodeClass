92b91fb8165bee9db5714cbfc07435a6cac04fad
Anthony Early <antearly925@gmail.com>
2024-03-04 19:18:02 -0800

    added icons

--- codeclass/src/App.js
+++ codeclass/src/App.js
@@ -4,11 +4,22 @@ import HomePage from './View/HomePage';
 import Projects from './View/Projects';
 import Apply from './View/Apply';
 import { Routes, Route } from 'react-router-dom'
+import { Helmet } from 'react-helmet';
+import logo from '../src/Pictures/logo.png';
 
 
 function App() {
   return (
     <div className=" bg-info">
+      <Helmet>
+        <title>Your Website Title</title>
+        <meta name="description" content="Description of your website" />
+        <meta property="og:title" content="Your Website Title" />
+        <meta property="og:description" content="Description of your website" />
+        <meta property="og:image" content={logo} /> {/* Image URL */}
+        <meta property="og:url" content="Your website URL" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Helmet>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<Projects />} />
